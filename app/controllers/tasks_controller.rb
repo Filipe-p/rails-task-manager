99596903a4e749d1bff5952a4c70deb5d858eb86
filipe paiva <filipe.p@soltroiavillas.com>
2017-02-15 11:16:55 +0000

    files from yesterday - unfinished files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,30 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def update
+    @task_params = Task.find(params[:id])
+    Task.update(@task_params)
+  end
+
+  def create
+    @new_task = Task.create(title: params[:task][:title], description: params[:task][:description], done: params[:task][:done])
+    redirect_to tasks_path
+  end
+
+  def destroy
+
   end
 end
+
+private
+  #params checking
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
-
-
   get 'tasks', to: 'tasks#index'
+
+  get 'tasks/:id', to: 'tasks#show'
+
+  get 'new', to: 'tasks#new'
+
+  post 'tasks', to: 'tasks#create'
+
+  # patch 'tasks/:id', to: 'tasks#show
 
 end
